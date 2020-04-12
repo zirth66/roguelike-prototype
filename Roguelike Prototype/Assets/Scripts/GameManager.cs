2d8120de513968f4f22cbef2850c04ee69f160f3
zirth <zirth66@gmail.com>
2020-04-12 20:53:41 +0200

    Tutorial part 2
    Added Dungeon Generator script, Map Manager script, and Game Manager script. Added Monospaced font for ascii graphics

---
+++ Roguelike Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    DungeonGenerator dungeonGenerator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dungeonGenerator = GetComponent<DungeonGenerator>();
+
+        dungeonGenerator.InitializeDungeon();
+        dungeonGenerator.GenerateDungeon();
+    }
+
+}
--- Roguelike Prototype/Assets/Scripts/MapManager.cs
+++ Roguelike Prototype/Assets/Scripts/MapManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System; //So the script can use the serialization commands
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,9 +8,38 @@ public class MapManager
     public static Tile[,] map; // the 2-dimensional map with the information for all the tiles
 }
 
+[Serializable] //Makes the class serializable so it can be saved out to a file
 public class Tile // holds all the information for each tile on the map
 {
     public int xPosition; // the position on the x axis
     public int yPosition; // the position on the y axis
+    [NonSerialized]
     public GameObject baseObject; // the map game object attached to that position: a floor, a wall etc.
+    public string type; // the type of the tile, if it is a wall, floor etc
+}
+
+[Serializable]
+public class Position // a class that saves the position of any cell
+{
+    public int x;
+    public int y;
+}
+
+[Serializable]
+public class Wall // a class for saving the wall information, for the dungeon generation algorithm
+{
+    public List<Position> positions;
+    public string direction;
+    public int length;
+    public bool hasFeature = false;
+}
+
+[Serializable]
+public class Feature // a class for saving the feature (corridor or room) information, for the dungeon generation algorithm
+{
+    public List<Position> positions;
+    public Wall[] walls;
+    public string type;
+    public int width;
+    public int height;
 }
